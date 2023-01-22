aeac96178b2a0257cd908c4b384812db53f027cc
Cheese McCheese <52334071+CheeseLad@users.noreply.github.com>
2023-01-22 21:00:32 +0000

    new problems

---
+++ isithalloween.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+s = input()
+
+if s == "OCT 31" or s == "DEC 25":
+  print("yup")
+else:
+  print("nope")
---
+++ twosum.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+import sys
+
+nums = sys.stdin.readline().strip().split()
+
+print(int(nums[0]) + int(nums[1]))
